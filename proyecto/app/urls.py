865be7a492ce31fd10118dbc82ca7346faf6ac6a
mpazdonicelli <pachudonicelli@hotmail.com>
2023-05-08 19:15:02 -0300

    Agregadas para cada modelo: las templates, las views y las url

--- proyecto/app/urls.py
+++ proyecto/app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from app import views
 from proyecto.views import saludar
+from app.views import inicio, materias, alumnos, profesores
 
 urlpatterns = [
     path('saludo/', saludar),
+    path('inicio/', inicio, name="inicio"),
+    path('materias/', materias, name="materias"),
+    path('alumnos/', alumnos, name="alumnos"),
+    path('profesores/', profesores, name="profesores")
 ]
--- proyecto/app/views.py
+++ proyecto/app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def inicio(request):
+    return render(request, "app/index.html")
+
+def materias(request):
+    return render(request, "app/materias.html")
+
+def alumnos(request):
+    return render(request, "app/alumnos.html")
+
+def profesores(request):
+    return render(request, "app/profesores.html")
