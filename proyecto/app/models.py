251dec791bd0660e5f9de4967870fd1f018affe3
mpazdonicelli <pachudonicelli@hotmail.com>
2023-05-08 18:46:38 -0300

    Agregados los models: materia, alumno y profesor

--- proyecto/app/models.py
+++ proyecto/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Materia(models.Model):
+    nombre=models.CharField(max_length=50)
+    año=models.IntegerField()
+
+class Alumno(models.Model):
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    email=models.EmailField()
+
+
+class Profesor(models.Model):
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    email=models.EmailField()
