bc32c9dabca1d8dc46b2df500f7711ab6ad1caa2
mpazdonicelli <pachudonicelli@hotmail.com>
2023-05-11 17:27:58 -0300

    Agregado el superuser

--- proyecto/app/admin.py
+++ proyecto/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Alumno, Profesor, Materia
 
 # Register your models here.
+admin.site.register(Alumno)
+admin.site.register(Profesor)
+admin.site.register(Materia)
+
+
+# usuario: pachu
+# contraseña: 151099
